24ca89018bde16c88277f4f9d607cf9d66b3a5c2
Fahad Shaikh <fahadshaikh6160@gmail.com>
2023-12-13 15:08:13 +0530

    My new portfolio website

---
+++ script.js
@@ -0,0 +1,66 @@
+function locomotive(){
+    gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector(".wrapper"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".wrapper" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".wrapper", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".wrapper").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+}
+locomotive();
+
+
+function role(){
+  var typeData = new Typed(".role", {
+    strings: [
+        "Software Developer.",
+        "Full Stack Developer.",
+        "Web Developer.",
+        "MERN Stack Dev.",
+        "UI-UX Designer.",
+        "Backend Developer.",
+        "Coder.",
+    ],
+    loop: true,
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    });
+}
+role();
+
+
+const vid = document.querySelectorAll(".video");
+
+for(let i=0; i<vid.length; i++){
+  vid[i].addEventListener("mouseover", function(e){
+    console.log("playing")
+    vid[i].play();
+  })
+
+  vid[i].addEventListener("mouseout", function(e){
+    console.log("pausing")
+    vid[i].pause();
+  })
+}
